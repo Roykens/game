4a6e4a4f0ad4ace69d0d72407d1e29251807685b
敖晓强 <=>
2016-04-23 13:30:07 +0800

    update user-buy

--- src/user-buy/user-buy.js
+++ src/user-buy/user-buy.js
@@ -15,22 +15,50 @@ angular.module('App.User.Buy', []).controller('App.User.Buy.Controller', [
     ) {
         $ionicSlideBoxDelegate.update();
         $scope.back = function() { $ionicHistory.goBack(); }
-        // console.log($state.params.id);
+
+
+        // 获取套餐
+        $scope.getCombos = function(arr){
+            var combos_list = [];
+            for (var i = 0; i < arr.length; i++) {
+                combos_list.push(arr['packageprice_set'][i]['combo']);
+            }
+            // debugger;
+            $scope.combos_list = combos_list;
+        }
+
+        // 获取颜色
+        $scope.getColors = function(arr){
+            var colors_list = [];
+            for (var i = 0; i < arr.length; i++) {
+                colors_list.push(arr['packageprice_set'][i]['color']);
+            }
+            $scope.colors_list = colors_list;
+        }
+
+        // 获取芯片信息列表
         Chips.getChipsList().$promise.then(function(response) {
             $scope.chips_list = response;
-            $scope.chip = ($scope.chips_list)[0];
-
-
-        console.log($scope.chip);
-        }, function(response){
-            console.log(response);
+            $scope.chip = $scope.chips_list[0];
+            $scope.getCombos($scope.chip);
+            $scope.getColors($scope.chip);
+            console.log( $scope.combos_list );
+        }, function(response) {
+            // console.log(response);
         })
 
         // 选择套餐
         $scope.choseType = 1;
-        $scope.selectType = function(index){
-        	$scope.choseType = index;
-        	console.log($scope.choseType);
+        $scope.selectType = function(index) {
+            $scope.choseType = index;
+            console.log($scope.choseType);
+        }
+
+        // 选择颜色
+        $scope.choseColor = 1;
+        $scope.selectColor = function(index) {
+            $scope.choseColor = index;
+            console.log($scope.choseColor);
         }
 
         // tab切换
@@ -38,5 +66,6 @@ angular.module('App.User.Buy', []).controller('App.User.Buy.Controller', [
         $scope.setTabs = function(index) {
             $scope.activeTab = index;
         }
+
     }
 ]);
